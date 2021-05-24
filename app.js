fafee7e42146ca6bf7b37919b632d41f9c6560cf
Rotem Yakir <yakir.rotem@gmail.com>
2021-05-24 23:46:30 +0300

    Clean up and fix bug

--- app.js
+++ app.js
@@ -4,20 +4,26 @@ const Contract = require('web3-eth-contract');
 Contract.setProvider(provider);
 const web3 = new Web3(provider);
 
+//--- Change if needed
+const gas = 1000000;
+const gasPrice = Web3.utils.toWei("7", "shannon")
+//---
+
+//--- Change once
+const yourAddress = '0xCADfB37bDADb4a5D486cfb1CaCbAA76E54e1F2c5';
+const yourPrivateKey = require('./private-key').key;
+//---
+
+//--- Change every pump and dump
+const shitTokenAddress = '0x3f8470484286715537af24eb5362aeeefd58606f';
+const bnbAmountToBuy = "0.001";
+//---
+
 const max = "115792089237316195423570985008687907853269984665640564039457584007913129639935";
-
-//---
-const shitTokenAddress = '0xAC51066d7bEC65Dc4589368da368b212745d63E8';
-//---
-
 const pancakeSwapAddress = '0x10ED43C718714eb63d5aA57B78B54704E256024E';
 const wBNB = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
-const yourAddress = '0xCADfB37bDADb4a5D486cfb1CaCbAA76E54e1F2c5';
-const yourPrivateKey = 'XXX';
-
-const pancakeswapRouterContract = new Contract(require('./pancakeswap-router-abi'), pancakeSwapAddress);
 const shitTokenContract = new Contract(require('./shit-token-abi'), shitTokenAddress);
-const bnbAmountToBuy = "0.001";
+const pancakeswapRouterContract = new Contract(require('./pancakeswap-router-abi'), pancakeSwapAddress);
 
 const swapBNBToShitToken = async () => {
 
@@ -31,40 +37,19 @@ const swapBNBToShitToken = async () => {
         new Date().getTime() + (60 * 1000) // 1 hour from now
     ).encodeABI();
 
-    const tx = {
-        // this could be provider.addresses[0] if it exists
-        from: yourAddress,
-        // target address, this could be a smart contract address
-        to: pancakeSwapAddress,
-        // optional if you want to specify the gas limit
-        gas: 500000,
-        gapPrice: Web3.utils.toWei("7", "shannon"),
-        // optional if you are invoking say a payable function
-        value: Web3.utils.toWei(bnbAmountToBuy, "ether"),
-        // this encodes the ABI of the method and the arguements
-        data: data
-    };
+    const tx = signTransaction(yourAddress, pancakeSwapAddress, Web3.utils.toWei(bnbAmountToBuy, "ether"), data);
 
-    const signedTransaction = await web3.eth.accounts.signTransaction(
-        tx,
-        yourPrivateKey
-    );
-
-    await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction).on('transactionHash', function(hash){
-        console.log("transactionHash", hash);
-    }).on('receipt', function(receipt){
-        console.log("receipt - swapBNBToShitToken");
-    }).on('error', console.error); // If a out of gas error, the second parameter is the receipt.;;
+    await signAndTransmitTransaction(tx);
 
 };
 
-const swapShitTokenToBNB = async () => {
+const swapShitTokenToBNB = async (withFee) => {
 
     const amountOfShitToken = await shitTokenContract.methods.balanceOf(yourAddress).call();
 
     console.log("blanace of", amountOfShitToken);
 
-    const data = pancakeswapRouterContract.methods.swapExactTokensForETH(
+    const data = pancakeswapRouterContract.methods[withFee ? 'swapExactTokensForETHSupportingFeeOnTransferTokens' : 'swapExactTokensForETH'](
         amountOfShitToken,
         1, // Min amount. 1 means 100% slippage
         [
@@ -75,30 +60,9 @@ const swapShitTokenToBNB = async () => {
         new Date().getTime() + (60 * 1000) // 1 hour from now
     ).encodeABI();
 
-    const tx = {
-        // this could be provider.addresses[0] if it exists
-        from: yourAddress,
-        // target address, this could be a smart contract address
-        to: pancakeSwapAddress,
-        // optional if you want to specify the gas limit
-        gas: 500000,
-        gapPrice: Web3.utils.toWei("7", "shannon"),
-        // optional if you are invoking say a payable function
-        value: 0,
-        // this encodes the ABI of the method and the arguements
-        data: data
-    };
+    const tx = signTransaction(yourAddress, pancakeSwapAddress, 0, data);
 
-    const signedTransaction = await web3.eth.accounts.signTransaction(
-        tx,
-        yourPrivateKey
-    );
-
-    await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction).on('transactionHash', function(hash){
-        console.log("transactionHash", hash);
-    }).on('receipt', function(receipt){
-        console.log("receipt - swapShitTokenToBNB");
-    }).on('error', console.error); // If a out of gas error, the second parameter is the receipt.;;
+    await signAndTransmitTransaction(tx);
 
 };
 
@@ -109,31 +73,40 @@ const approveShitToken = async () => {
         max // Infinite
     ).encodeABI();
 
-    const tx = {
-        // this could be provider.addresses[0] if it exists
-        from: yourAddress,
-        // target address, this could be a smart contract address
-        to: shitTokenAddress,
-        // optional if you want to specify the gas limit
-        gas: 500000,
-        gapPrice: Web3.utils.toWei("7", "shannon"),
-        // optional if you are invoking say a payable function
-        value: 0,
-        // this encodes the ABI of the method and the arguements
-        data: data
-    };
+    const tx = signTransaction(yourAddress, shitTokenAddress, 0, data);
+
+    await signAndTransmitTransaction(tx);
+};
+
+const signAndTransmitTransaction = async (tx) => {
 
     const signedTransaction = await web3.eth.accounts.signTransaction(
         tx,
         yourPrivateKey
     );
 
-    await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction).on('transactionHash', function(hash){
+    await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction).on('transactionHash', function (hash) {
         console.log("transactionHash", hash);
-    }).on('receipt', function(receipt){
+    }).on('receipt', function (receipt) {
         console.log("receipt - approveShitToken");
     }).on('error', console.error); // If a out of gas error, the second parameter is the receipt.;;
 
+};
+
+const signTransaction = (from, to, value, data) => {
+    return {
+        // this could be provider.addresses[0] if it exists
+        from: from,
+        // target address, this could be a smart contract address
+        to: to,
+        // optional if you want to specify the gas limit
+        gas: gas,
+        gapPrice: gasPrice,
+        // optional if you are invoking say a payable function
+        value: value,
+        // this encodes the ABI of the method and the arguements
+        data: data
+    };
 };
 
 (async () => {
@@ -142,7 +115,7 @@ const approveShitToken = async () => {
     //await swapBNBToShitToken();
 
     // 2
-    //await approveShitToken();
-    //await swapShitTokenToBNB();
+    // await approveShitToken();
+    // await swapShitTokenToBNB(true);
 
 })();
